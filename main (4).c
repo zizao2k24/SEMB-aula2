8fb695b9ed1e083db85708202ff5700afa0e8b57
zizao2k24 <00001132668463sp@al.educacao.sp.gov.br>
2026-08-07 14:49:12 -0300

    Add files via upload
    programa de cálculo de média

---
+++ main (4).c
@@ -0,0 +1,23 @@
+/**/
+#include <stdio.h>
+float M, A, B;
+float number = 3.14;
+
+int main()
+{
+  while (1)
+  {
+      printf("\n\nPrograma de soma. \n");
+      printf("entre com o primeiro numero: ");
+      scanf("%f", &A);
+      printf("entre com o segundo numero: ");
+      scanf("%f", &B);
+      M = (A + B)/2;
+      printf("sua média é: %.2f", M);
+      if (M < 50) printf("você foi reprovado.\n\n");
+      if(M > 50) printf("que arraso, você passou");
+      
+      
+     
+  }
+}
