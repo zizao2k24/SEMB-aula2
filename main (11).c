04056cd8a34222813dab4cd4847c279c519200ce
zizao2k24 <00001132668463sp@al.educacao.sp.gov.br>
2026-08-10 17:10:29 -0300

    Add files via upload
    idade

---
+++ main (11).c
@@ -0,0 +1,20 @@
+/* programa for */
+#include <stdio.h>
+
+int ano_nasc, ano_atual;
+char aniver;
+
+int main()
+{
+  printf("calculador de idade.\n");
+  printf("em que ano você nasceu?. ");
+  scanf("%i", &ano_nasc);
+  printf("já fez aniversário este ano?. ");
+  scanf("%s", &aniver);
+  if(aniver == 's')
+  {
+      
+     
+  }
+  }
+}
